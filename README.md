2558837e9ece8ef435932e3d202453a07eb69912
Gucci <uegco72@gmail.com>
2022-05-21 18:41:50 -0500

    upload

---
+++ README.md
@@ -0,0 +1,5 @@
+# Knockout
+Simple Knockout Script for QB-Core.
+
+## Credits 
+[Zotactiks](https://github.com/Zotactiks/Knockout)
---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Zotactiks#0001, modified by Gucci#1966'
+description 'Knockout'
+version '1.0.0'
+
+
+
+client_scripts {
+	'unarmed_knockout.lua',
+} 
